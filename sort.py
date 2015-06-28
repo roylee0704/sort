0c94500b80a4245083c9426fe4089be051779e01
roylee0704 <roylee0704@gmail.com>
2015-06-28 14:45:17 +0800

    initial commit

---
+++ sort.py
@@ -0,0 +1,24 @@
+"""
+	Sorting is arranging the elements in a list or collection in increasing or decreasing
+	order of some property. 
+
+	I.e: Sort by order of `value`, sort by order of factor
+
+"""
+
+
+def sortByOrderOfFactor(list):
+	print list
+
+
+
+def main():
+	num_list = [2, 3, 9, 4, 6]
+
+	sortByOrderOfFactor(num_list)
+
+
+
+
+if __name__ == '__main__':
+	main()
